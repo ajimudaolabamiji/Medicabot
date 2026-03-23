8b0b562570d8eb1d867ab576b7898e490e4b2dd5
Olabamiji Ajimuda <ajimudaolabamiji@gmail.com>
2026-03-23 16:11:09 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author ="Olabamiji Ajimuda",
+    author_email = "ajimudaolabamiji@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+
+)
